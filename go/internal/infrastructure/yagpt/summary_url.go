283dc50ae4be2deffc8de962f60ecc35d278ea5a
Kirill Plotnikov <init@pltnk.dev>
2025-08-23 20:50:54 +0500

    Add SummaryURL struct

---
+++ go/internal/infrastructure/yagpt/summary_url.go
@@ -0,0 +1,54 @@
+package yagpt
+
+import (
+	"fmt"
+	"net/url"
+	"path"
+	"strings"
+)
+
+const baseHostname = "300.ya.ru"
+
+type SummaryURL struct {
+	url   url.URL
+	token string
+}
+
+func (su *SummaryURL) URL() url.URL {
+	return su.url
+}
+
+func (su *SummaryURL) Token() string {
+	return su.token
+}
+
+func (su *SummaryURL) String() string {
+	return su.url.String()
+}
+
+func NewSummaryURL(rawURL string) (*SummaryURL, error) {
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		return nil, fmt.Errorf("creating summary URL from %q: %w", rawURL, err)
+	}
+
+	if !strings.EqualFold(u.Scheme, "https") {
+		return nil, fmt.Errorf("creating summary URL from %q: want Scheme=%q, got %q", rawURL, "https", u.Scheme)
+	}
+
+	if !strings.EqualFold(u.Hostname(), baseHostname) {
+		return nil, fmt.Errorf("creating summary URL from %q: want Hostname=%q, got %q", rawURL, baseHostname, u.Hostname())
+	}
+
+	t := path.Base(u.Path)
+	if t == "" || t == "/" || t == "." {
+		return nil, fmt.Errorf("creating summary URL from %q: no token", u.String())
+	}
+
+	su := &SummaryURL{
+		url:   *u,
+		token: t,
+	}
+
+	return su, nil
+}
